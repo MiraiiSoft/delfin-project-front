928e31bc9ff28817535fbab3b18f592aae418af0
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2023-07-14 11:46:55 -0600

    empezando hacer modulo de usuario

---
+++ src/app/user/user-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeuserComponent } from './pages/homeuser/homeuser.component';
+import { DatosUserComponent } from './components/datos-user/datos-user.component';
+import { ComprasComponent } from './components/compras/compras.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeuserComponent,
+    children: [
+      {
+        path: 'datos',
+        component: DatosUserComponent
+      },
+      {
+        path: 'compras',
+        component: ComprasComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild( routes )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class UserRoutingModule { }
