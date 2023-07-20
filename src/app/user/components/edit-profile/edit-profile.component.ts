a2defbca3319dde3fd01b9a9384d64da41e28b57
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2023-07-20 11:14:37 -0600

    hecho vista para editar direccion

--- src/app/user/components/edit-profile/edit-profile.component.ts
+++ src/app/user/components/edit-profile/edit-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { EditAddressComponent } from '../edit-address/edit-address.component';
 
 @Component({
   selector: 'app-edit-profile',
@@ -11,6 +12,28 @@ export class EditProfileComponent implements OnInit {
   imgLogo = "assets/img/auth/LogoPapeleria.png";
   prefijos = [ "+52" ];
 
+  dataPersona = {
+    id_login: 1,
+    correo: "juan@example.com",
+    usuario: "juan123",
+    password: "password1",
+    is_verified: false,
+    id_persona: 1,
+    id_roll: 1,
+    is_active: false,
+    persona: {
+      id_persona: 1,
+      nombre: "Juan",
+      apellido: "Pérez",
+      telefono: "555-1234",
+      id_direccion: 1
+    },
+    roll: {
+      id_roll: 1,
+      roll: "Admin"
+    }
+  };
+
   constructor( private dialog: MatDialog ) { }
 
   ngOnInit(): void {
@@ -18,6 +41,14 @@ export class EditProfileComponent implements OnInit {
 
   closeModal(){
     this.dialog.closeAll()
+  }
+
+  openEditAddress(){
+    this.dialog.open( EditAddressComponent, {
+      data: {
+        nombrePersona: this.dataPersona.persona.nombre
+      }
+    } );
   }
 
 
