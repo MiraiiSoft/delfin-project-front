26e5a3e0867f6e192058885f6012968a97235124
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2023-07-27 13:58:08 -0600

    relalizando ajustes en header y login

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -23,8 +23,8 @@ export class LoginComponent implements OnInit, OnDestroy {
   constructor( private fb: FormBuilder, private router: Router, private authServie: AuthService, private dialog: MatDialog ) { }
 
   loginForm: FormGroup = this.fb.group({
-    user: [ '', [ Validators.required ] ],
-    pass: [ '', [ Validators.required, Validators.minLength(5) ] ]
+    user: [ 'prueba1', [ Validators.required ] ],
+    pass: [ 'prueba123', [ Validators.required, Validators.minLength(5) ] ]
   });
 
   ngOnInit(): void {
